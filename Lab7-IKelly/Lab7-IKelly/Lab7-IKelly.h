832452e3030d41b663b00f34ef4c4f8068cc2681
iankellyUW <43155493+iankellyUW@users.noreply.github.com>
2018-11-13 15:54:02 -0700

    Add files via upload

---
+++ Lab7-IKelly/Lab7-IKelly/Lab7-IKelly.h
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <iterator>
+
+#ifndef HEADERS_H_
+#define HEADERS_H_
+
+class Sort {
+public:
+	void bubble(vector<double> &list);
+
+	void insertion() {};
+	void selection() {};
+	void merge() {};
+	void quickSort() {};
+};
+#endif
